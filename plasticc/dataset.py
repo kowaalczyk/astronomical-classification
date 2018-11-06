4d76987b66971c919d0042101f4d8f82388cc963
Michał Filipiuk <mkfilipiuk@gmail.com>
2018-11-07 00:49:29 +0100

    Created Dataset wrapper to ease loading csv.
    [WIP] Added changes in logic of Dataset class; added Dataset_manager; NOT YET TESTED
    
    Some fixes. Ready for PR

--- plasticc/dataset.py
+++ plasticc/dataset.py
@@ -9,7 +9,6 @@ from tqdm.autonotebook import tqdm
 class DatasetException(Exception):
     pass
 
-
 class Dataset(object):
     """
     Dataset wrapper for easier path management in scripts.
@@ -18,48 +17,33 @@ class Dataset(object):
     (= metadata is in separate CSVs like in the raw data),
     the class provides access to metadata CSVs.
     """
+    
     def __init__(self, path: str):
         self.path = path
-
-    def has_meta(self):
-        return os.path.exists(os.path.join(self.path, 'meta/'))
-
-    @classmethod
-    def with_structure(dataset_class, path: str, has_meta=False):
-        """
-        Creates an empty dataset directory structure in provided path and 
-        returns a dataset wrapper for this path.
-        """
-        os.makedirs(os.path.join(path, 'test/'))
-        if has_meta:
-            os.makedirs(os.path.join(path, 'meta/'))
-        return dataset_class(path)
-
-    def meta_path(self, csv_name: str) -> str:
-        if not self.has_meta():
-            raise DatasetException("Dataset has no metadata!")
-        return os.path.join(self.path, 'meta/', csv_name)
-
+        self._data = None
+        self._X = None
+        self._y = None
+        
     @property
-    def train_meta_df(self) -> pd.DataFrame:
-        df = pd.read_csv(self.meta_path('train.csv'))
-        df.index = df['object_id']
-        return df
- 
+    def values(self) -> pd.DataFrame:
+        return self._data
+    
     @property
-    def test_meta_df(self) -> pd.DataFrame:
-        df = pd.read_csv(self.meta_path('test.csv'))
-        df.index = df['object_id']
-        return df
-
+    def X(self) -> pd.DataFrame:
+        return self._X
+    
+    @property
+    def y(self) -> pd.DataFrame:
+        return self._y
+    
     @property
     def train_path(self) -> str:
         return os.path.join(self.path, 'train.csv')
-
+    
     @property
     def test_path(self) -> str:
         return os.path.join(self.path, 'test/')
-
+    
     @property
     def test_paths(self) -> List[str]:
         test_file_names = sorted(os.listdir(self.test_path))
@@ -77,40 +61,116 @@ class Dataset(object):
 
 def batch_data(
         ts_reader: pd.io.parsers.TextFileReader, 
+    
+    @property
+    def iter_test_dfs(self):
+        for csv_name in tqdm(self.test_paths):
+            yield Dataset(csv_name).train_df()
+    
+    def has_meta(self):
+        return os.path.exists(os.path.join(self.path, 'meta/'))
+    
+    def meta_path(self, csv_name: str) -> str:
+        if not self.has_meta():
+            raise DatasetException("Dataset has no metadata!")
+        return os.path.join(self.path, 'meta/', csv_name)
+    
+    def train_meta_df(self, y=None):
+        if self._data is not None:
+            raise DatasetException("This dataset has already set data!")
+        if y is None:
+            raise DatasetException("You have to specify y column name in dataset!")
+        self._data = pd.read_csv(self.meta_path('train.csv'))
+        self._data.index = self._data['object_id']
+        if y not in _data.columns:
+            raise DatasetException("Specified y column name does not exist in dataset!")
+        self._X = self._data.loc[:, data.columns != y]
+        self._y = self._data.loc[:, data.columns == y]
+        return self
+    
+    def test_meta_df(self) -> pd.DataFrame:
+        if self._data is not None:
+            raise DatasetException("This dataset has already set data!")
+        self._data = pd.read_csv(self.meta_path('test.csv'))
+        self._data.index = self._data['object_id']
+        self._X = self._data 
+        return self
+    
+    def train_df(self, y=None):
+        if self._data is not None:
+            raise DatasetException("This dataset has already set data!")
+        if y is None:
+            raise DatasetException("You have to specify y column name in dataset!")
+        self._data = pd.read_csv(self.train_path)
+        self._data.index = self._data['object_id']
+        if y not in _data.columns:
+            raise DatasetException("Specified y column name does not exist in dataset!")
+        self._X = self._data.loc[:, data.columns != y]
+        self._y = self._data.loc[:, data.columns == y]
+        return self
+
+class Dataset_manager(object):
+    """
+    Static class creating a dataset directory structure and an instance 
+    """
+    @staticmethod
+    def get_dataset_with_structure(path: str, has_meta=False):
+        """
+        Creates an empty dataset directory structure in provided path and 
+        returns a Dataset for this path.
+        """
+        os.makedirs(os.path.join(path, 'test/'))
+        if has_meta:
+            os.makedirs(os.path.join(path, 'meta/'))
+        return Dataset(path)            
+            
+
+def batch_data(
+        ts_reader: pd.io.parsers.TextFileReader, 
+        meta_df: pd.DataFrame=None, 
         output_dir='../data/sets/base/test/', 
         lines=453653105
     ):
     """
     Splits pd.DataFrame iterated with ts_reader into batches.
     Each batch is saved as csv file in output_dir.
+    If meta_df is provided, each record in time series is joined 
+    with corresponding object's metadata (making the process ~3x slower).
     Lines argument is necessary for progressbar to work correctly.
     """
     reminder_df = pd.DataFrame()
     with tqdm(total=lines) as progressbar:
         for batch in ts_reader:
+            if meta_df is not None:
+                batch = batch.join(
+                    meta_df, 
+                    on='object_id', 
+                    rsuffix='meta_', 
+                    sort=True
+                )
             # prepend reminder of rows from previous iteration
             if len(reminder_df) > 0:
                 current_df = pd.concat([reminder_df, batch], axis=0)
             else:
                 current_df = batch
-            current_df.sort_values(by=['object_id', 'passband', 'mjd'], inplace=True)
+            current_df.sort_values(by=['object_id', 'mjd', 'passband'], inplace=True)
             # separate reminder for next iteration and save current batch
             last_id = current_df.iloc[-1]['object_id']
             reminder_df = current_df[current_df['object_id'] == last_id]
             save_df = current_df[current_df['object_id'] != last_id]
-            save_batch(save_df, output_dir)
+            _save_batch(save_df, output_dir)
             progressbar.update(len(save_df))
         # save last reminder
         if len(reminder_df) > 0:
-            save_batch(reminder_df, output_dir)
+            _save_batch(reminder_df, output_dir)
             progressbar.update(len(reminder_df))
 
 
-def save_batch(batch: pd.DataFrame, output_dir: str):
+def _save_batch(batch: pd.DataFrame, output_dir: str):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     save_filename = _name_for_batch(batch)
-    batch.to_csv(os.path.join(output_dir, save_filename), index=False)
+    batch.to_csv(os.path.join(output_dir, save_filename))
 
 
 def _name_for_batch(batch: pd.DataFrame, pad_to_length=12) -> str:
