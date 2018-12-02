a70f2f1da522fcb590ec0611e210a72b4752a27e
radrow <radrowicki@gmail.com>
2018-12-02 12:54:15 +0100

    Sanity

--- plasticc/train.py
+++ plasticc/train.py
@@ -5,6 +5,7 @@ from sklearn.ensemble import BaggingClassifier
 from sklearn.model_selection import train_test_split
 
 from plasticc.dataset import Dataset
+import numpy as np
 
 
 random_seed = 2222
@@ -28,14 +29,14 @@ def build_xgb():
 
 def build_bagged_model(base_estimator):
     return BaggingClassifier(
-        base_estimator=base_estimator
-        n_estimators=20, 
-        max_samples=0., 
-        max_features=0.67, 
-        bootstrap=True, 
-        bootstrap_features=True, 
-        oob_score=True, 
-        n_jobs=-1, 
+        base_estimator=base_estimator,
+        n_estimators=20,
+        max_samples=0.,
+        max_features=0.67,
+        bootstrap=True,
+        bootstrap_features=True,
+        oob_score=True,
+        n_jobs=-1,
         random_state=random_seed
     )
 
@@ -45,18 +46,18 @@ def train_bagging_model(
         dataset: Dataset,
         output_path: str,
         base_estimator,
-        test_frac: float=0.1
+        test_frac: float = 0.1
 ):
     X, y = dataset.train
     X_train, X_test, y_train, y_test = train_test_split(test_size=test_frac, random_state=random_seed)
-    
+
     bc = build_bagged_model(base_estimator=build_xgb())
     bc.fit(X_train.values.astype(np.float32), y_train.values.astype(np.int))
-    
+
     print("Accuracy score on validation set: ", bc.score(X_test, y_test))
     _save_model_if_path_not_none(bc, output_path)
 
 def _save_model_if_path_not_none(model, path):
-    if output_path is not None:
-        pickle.dump(model, open(output_path, "wb"))
+    if path is not None:
+        pickle.dump(model, open(path, "wb"))
     return model
