e035d6b2eaa759ef58d9577b73658933937d7668
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2018-12-02 19:16:01 +0100

    Model and loss functions development, fix train_xgb script

--- plasticc/metrics.py
+++ plasticc/metrics.py
@@ -1,5 +1,6 @@
 from sklearn.model_selection import ShuffleSplit, cross_val_score
 from sklearn.metrics import make_scorer
+from sklearn.preprocessing import OneHotEncoder
 from xgboost import XGBClassifier
 import numpy as np
 from plasticc.dataset import Dataset
@@ -28,6 +29,7 @@ def kaggle_scoring():
     return make_scorer(score_func=kaggle_loss, greater_is_better=False)
 
 def kaggle_loss(y, y_pred, **kwargs):
+    # print(f"loss on y: {y.shape} and y_pred: {y_pred.shape}")
     """ Implemented multi log loss from kaggle competition """
     #Analised weights for classes
     magic_const = 1.9188
@@ -46,13 +48,61 @@ def kaggle_loss(y, y_pred, **kwargs):
       "class_90": 1,
       "class_92": 1,
       "class_95": 1,
-      "class_99": 2,
+    #  "class_99": 2,
     }
 
     #evading log extremes
     min_values = (np.ones(len(y_pred)*len(weight_dict))*(1-1e-15)).reshape(len(y_pred),len(weight_dict))
     removed_ones = np.minimum(y_pred,min_values)
     removed_zeros = np.maximum(removed_ones, (1e-15))
+    y_pred = removed_zeros
+
+    #calculating loss
+    ln_p = np.log(y_pred)
+    N = np.array(np.sum(y, axis=0))
+    weights = np.array(list(weight_dict.values()))
+    y_with_ln = np.multiply(y,ln_p)
+    multiplied_weights = np.multiply(weights,y_with_ln/N)
+    #removing nan in case of not represented classes (in that case Ni equals 0 and we get division by zero)
+    nan_removed = np.nan_to_num(multiplied_weights)
+    sum_from_j_to_Ni = np.sum(nan_removed, axis=1)
+    sum_from_i_to_M = np.sum(sum_from_j_to_Ni)
+    numerator = sum_from_i_to_M
+    denominator = sum(weight_dict.values())*magic_const
+    return -numerator/denominator
+
+def xgb_kaggle_loss(y_true, y_pred, **kwargs):
+    y_true = OneHotEncoder(sparse=False).fit_transform(y_true)
+    return kaggle_loss(y_true, y_pred, **kwargs)
+
+def wtf_xgb_kaggle_loss(y_pred, y_true, **kwargs):
+    """ Implemented multi log loss from kaggle competition (XGBoost version) """
+    #Analised weights for classes
+    magic_const = 1.9188
+    weight_dict = {
+      "class_6": 1,
+      "class_15": 2,
+      "class_16": 1,
+      "class_42": 1,
+      "class_52": 1,
+      "class_53": 1,
+      "class_62": 1,
+      "class_64": 2,
+      "class_65": 1,
+      "class_67": 1,
+      "class_88": 1,
+      "class_90": 1,
+      "class_92": 1,
+      "class_95": 1,
+     # "class_99": 2,
+    }
+    y = y_true.get_label()
+    y = OneHotEncoder(sparse=False).fit_transform(labels.reshape(-1, 1))
+
+    #evading log extremes
+    min_values = (np.ones(len(y_pred)*len(weight_dict))*(1-1e-15)).reshape(len(y_pred),len(weight_dict))
+    removed_ones = np.minimum(y_pred,min_values)
+    removed_zeros = np.maximum(np.minimum(y_pred,min_values), (1e-15))
     y_pred = removed_zeros
 
     #calculating loss
