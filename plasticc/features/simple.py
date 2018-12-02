d5aad039f6bd872f15a828e946d81f311889153a
Krzysztof Kowalczyk <kk385830@arnold.mimuw.edu.pl>
2018-12-02 12:42:35 +0100

    WIP features and xgb training based on kaggle kernel

--- plasticc/features/simple.py
+++ plasticc/features/simple.py
@@ -2,9 +2,20 @@ import os
 from shutil import copyfile
 from itertools import product
 
+import numpy as np
 import pandas as pd
 
 from plasticc.dataset import Dataset, save_batch, build_dataset_structure
+
+
+simple_aggregations = {
+    'mjd': ['min', 'max', 'mean', 'count'],  # this is not only time but also a relevant feature, as pointed out on the forum
+    'flux': ['min', 'max', 'mean', 'median', 'std', 'skew'],  # all relevant pandas aggregations except count (which is same for all columns and calculated for mjd)
+    'flux_err': ['min', 'max', 'mean', 'median', 'std', 'skew'],  # keep these same as for flux - might be useful for future transformations
+    'detected': ['mean'],  # this is binary so knowing mean and count translates to knowing how many actual samples were marked as detected
+    'flux_ratio_sq': ['min', 'max', 'sum', 'skew'],
+    'flux_by_flux_ratio_sq': ['min', 'max', 'sum', 'skew'],
+}
 
 
 def from_base(base_dataset: Dataset, out_dataset_path: str, process_test=True) -> Dataset:
@@ -34,18 +45,43 @@ def from_base(base_dataset: Dataset, out_dataset_path: str, process_test=True) -
 
 
 def _extract_features(series_df: pd.DataFrame, meta_df: pd.DataFrame) -> pd.DataFrame:
-    gbo = series_df.groupby(['passband', 'object_id'])
-    aggr_dfs = {
-        'min': gbo.mean(),
-        'max': gbo.max(),
-        'mean': gbo.mean()
-    }
-    result_column_names = aggr_dfs['min'].columns
-    passbands = range(0,6)
-    train_features_df = pd.DataFrame(index=series_df['object_id'].unique())
-    for passband, aggr_type, colname in product(passbands, aggr_dfs.keys(), result_column_names):
-        out_colname = f'passband_{passband}_{aggr_type}_{colname}'
-        train_features_df[out_colname] = aggr_dfs[aggr_type][colname].loc[passband]
-    meta_df.index = meta_df["object_id"]
-    train_out_df = train_features_df.join(meta_df, rsuffix='_meta')
-    return train_out_df
+    series_df = _with_series_features(series_df)
+    flat_aggr_df = _compute_aggregate_features(series_df)
+    flat_aggr_df = _with_differential_features(flat_aggr_df)
+    # TODO: Aggregating min, max, etc. across various passbands might be a good idea, implement it.
+    joined_df = flat_aggr_df.join(meta_df, rsuffix='_meta')
+    return joined_df.drop(columns=[col for col in set(joined_df.columns) if col.endswith('_meta')])
+
+
+def _with_series_features(series_df: pd.DataFrame) -> pd.DataFrame:
+    series_df['flux_ratio_sq'] = np.power(series_df['flux'] / series_df['flux_err'], 2.0)
+    series_df['flux_by_flux_ratio_sq'] = series_df['flux'] * series_df['flux_ratio_sq']
+    return series_df
+
+
+def _compute_aggregate_features(series_df: pd.DataFrame) -> pd.DataFrame:
+    aggr_df = series_df.groupby(['passband', 'object_id']).agg(simple_aggregations)
+    flattened_dfs = [_flatten_columns(aggr_df.xs(passband), f"passband_{passband}_") for passband in range(1,6)]
+    flattened_df = pd.concat(flattened_dfs, axis=1)
+    return flattened_df
+
+
+def _flatten_columns(df: pd.DataFrame, col_prefix: str) -> pd.DataFrame:
+    df.columns = [col_prefix + '_'.join(col).strip() for col in df.columns.values]
+    return df
+
+
+def _with_differential_features(flat_aggr_df: pd.DataFrame) -> pd.DataFrame:
+    # flux-related features
+    for passband in range(1,6):
+        for differential_colname in ['flux', 'flux_err']:
+            colname_base = f'passband_{passband}_{differential_colname}'
+            flat_aggr_df[f'{colname_base}_diff'] = flat_aggr_df[f'{colname_base}_max'] - flat_aggr_df[f'{colname_base}_min']
+            flat_aggr_df[f'{colname_base}_diff2'] = flat_aggr_df[f'{colname_base}_diff'] / flat_aggr_df[f'{colname_base}_mean']
+        flat_aggr_df[f'passband_{passband}_flux_w_mean'] = flat_aggr_df[f'passband_{passband}_flux_by_flux_ratio_sq_sum'] / flat_aggr_df[f'passband_{passband}_flux_ratio_sq_sum']
+        flat_aggr_df[f'passband_{passband}_flux_dif3'] = (flat_aggr_df[f'passband_{passband}_flux_max'] - flat_aggr_df[f'passband_{passband}_flux_min']) \
+                                                          / flat_aggr_df[f'passband_{passband}_flux_w_mean']
+        # other features
+        flat_aggr_df[f'passband_{passband}_detected_count'] = flat_aggr_df[f'passband_{passband}_detected_mean'] * flat_aggr_df[f'passband_{passband}_mjd_count']
+        flat_aggr_df[f'passband_{passband}_detected_mjd_diff'] = flat_aggr_df[f'passband_{passband}_mjd_max'] - flat_aggr_df[f'passband_{passband}_mjd_mean']
+    return flat_aggr_df
