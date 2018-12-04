cdc963459e0ffa6517ed39e3e1d00cbaf622bde6
Radosław Rowicki <35342116+radrow@users.noreply.github.com>
2018-12-04 20:57:26 +0100

    Prediction scripts (#42)
    * kek
    
    * minor fixes
    
    * runs
    
    * logging
    
    * sanity
    
    * resolvers

--- plasticc/train.py
+++ plasticc/train.py
@@ -1,30 +1,46 @@
 import pickle
 from typing import List
 
-from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
 import numpy as np
 import pandas as pd
 
-from plasticc.dataset import Dataset
-from xgboost import XGBClassifier
 from sklearn.ensemble import BaggingClassifier
 from sklearn.model_selection import train_test_split
-
-from plasticc.dataset import Dataset
-
+from plasticc.resolvers import resolve_dataset_name, resolve_model_name
 
 random_seed = 2222
 
 
 def train_model(
-        dataset: Dataset,
+        dataset_name: str,
         output_path: str,
-        model,
+        model_name: str,
         yname="target"
 ):
+
+    model = resolve_model_name(model_name)
+    dataset = resolve_dataset_name(dataset_name)
+
     X, y = dataset.train
-    model.fit(X.values, y.values)
+
+    X.fillna(0, inplace=True)
+    assert(X.notna().all().all())
+    X.drop(columns=[col for col in set(X.columns) if col.endswith('_meta')],
+           inplace=True)
+
+    print("Before infinity removal:", X.shape)
+    X.replace([np.inf, -np.inf], np.nan, inplace=True)
+    na_cols = null_values(X)
+    X.drop(columns=na_cols, inplace=True)
+    print("After infinity removal:", X.shape)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15,
+                                                        random_state=42)
+
+    print("Training!")
+    model.fit(X_train.values.astype(np.float32), y_train.values.astype(np.int))
+    print("Done.")
     _save_model_if_path_not_none(model, output_path)
     return model
 
@@ -39,30 +55,7 @@ def null_values(X: pd.DataFrame) -> List[str]:
     return na_cols
 
 
-def build_xgb(training_set: str):
-    if training_set == 'simple-2':
-        ds = Dataset('../data/sets/simple-12-01/', y_colname='target')
-    elif training_set == 'simple':
-        ds = Dataset('../data/sets/simple/', y_colname='target')
-    elif training_set == 'tsfresh':
-        ds = Dataset('../data/sets/tsfresh-sample/', y_colname='target')
-    else:
-        raise "No such dataset registered"
-
-    X, y = ds.train
-
-    X.fillna(0, inplace=True)
-    assert(X.notna().all().all())
-    X.drop(columns=[col for col in set(X.columns) if col.endswith('_meta')], inplace=True)
-
-    print("Before infinity removal:", X.shape)
-    X.replace([np.inf, -np.inf], np.nan, inplace=True)
-    na_cols = null_values(X)
-    X.drop(columns=na_cols, inplace=True)
-    print("After infinity removal:", X.shape)
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=42)
-
+def build_xgb():
     xgb_model = XGBClassifier(
         objective='multi:softmax',
         num_class=14,
@@ -74,15 +67,10 @@ def build_xgb(training_set: str):
         min_child_weight=10,
         n_estimators=1024,
         max_depth=3,
-        nthread=16
+        nthread=-1
     )
 
-    xgb_model.fit(X_train, y_train, verbose=100, eval_set=[(X_train, y_train), (X_test, y_test)], eval_metric='mlogloss', early_stopping_rounds=50)
-
-    print(f"Score: {xgb_model.score(X_test, y_test)}")
-
     return xgb_model
-
 
 
 def build_bagged_model(base_estimator):
@@ -98,24 +86,9 @@ def build_bagged_model(base_estimator):
         random_state=random_seed
     )
 
-# train_model(build_bagged_model(build_xgb()))
-
-def train_bagging_model(
-        dataset: Dataset,
-        output_path: str,
-        base_estimator,
-        test_frac: float = 0.1
-):
-    X, y = dataset.train
-    X_train, X_test, y_train, y_test = train_test_split(test_size=test_frac, random_state=random_seed)
-
-    bc = build_bagged_model(base_estimator=build_xgb())
-    bc.fit(X_train.values.astype(np.float32), y_train.values.astype(np.int))
-
-    print("Accuracy score on validation set: ", bc.score(X_test, y_test))
-    _save_model_if_path_not_none(bc, output_path)
 
 def _save_model_if_path_not_none(model, path):
     if path is not None:
         pickle.dump(model, open(path, "wb"))
+    print(f"Saved model to {path}")
     return model
