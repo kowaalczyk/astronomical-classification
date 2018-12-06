6d1847719e3dbcb363a6683133089534663fc2fc
radrow <radrowicki@gmail.com>
2018-12-06 16:06:13 +0100

    Test notebook

--- setup.py
+++ setup.py
@@ -5,12 +5,6 @@ setup(
     version='0.1',
     packages=find_packages(),
     entry_points={
-        'console_scripts': [
-            'plasticc-dataset-base = plasticc.scripts:create_base_dataset',
-            'plasticc-dataset-simple = plasticc.scripts:featurize_simple',
-            'plasticc-dataset-tsfresh = plasticc.scripts:featurize_tsfresh',
-            'plasticc-train = plasticc.scripts:train',
-            'plasticc-predict = plasticc.scripts:predict'
-        ]
+#         'console_scripts': []
     }
 )
